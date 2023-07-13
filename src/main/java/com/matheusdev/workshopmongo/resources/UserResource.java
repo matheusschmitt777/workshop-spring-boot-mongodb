f55ae102b417577e2ce7289bac7b4dba84c941a3
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-07-12 23:06:55 -0300

    Endpoint to return the posts of a user

--- src/main/java/com/matheusdev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/matheusdev/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheusdev.workshopmongo.dto.UserDTO;
+import com.matheusdev.workshopmongo.entities.Post;
 import com.matheusdev.workshopmongo.entities.User;
 import com.matheusdev.workshopmongo.services.UserService;
 
@@ -58,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
