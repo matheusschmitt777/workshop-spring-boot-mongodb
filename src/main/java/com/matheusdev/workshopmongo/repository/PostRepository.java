5d7ef85053a328e19dc9ad05ca1689be8d874e58
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-07-13 16:54:27 -0300

    Simple query with query methods

--- src/main/java/com/matheusdev/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/matheusdev/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.matheusdev.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.matheusdev.workshopmongo.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
