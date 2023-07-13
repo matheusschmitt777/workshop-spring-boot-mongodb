5a30b9d9f2db9e1358257a665d483476dae40e0e
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-07-12 23:34:23 -0300

    Adding comments to posts

--- src/main/java/com/matheusdev/workshopmongo/config/Instantiation.java
+++ src/main/java/com/matheusdev/workshopmongo/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.matheusdev.workshopmongo.dto.AuthorDTO;
+import com.matheusdev.workshopmongo.dto.CommentDTO;
 import com.matheusdev.workshopmongo.entities.Post;
 import com.matheusdev.workshopmongo.entities.User;
 import com.matheusdev.workshopmongo.repository.PostRepository;
@@ -34,8 +35,15 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null,  Instant.parse("2023-05-20T19:53:07Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, Instant.parse("2023-06-21T03:42:10Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post1 = new Post(null,  Instant.parse("2023-05-20T16:53:07Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, Instant.parse("2023-06-21T08:42:10Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", Instant.parse("2023-05-20T17:46:07Z"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", Instant.parse("2023-05-20T18:53:07Z"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", Instant.parse("2023-05-21T09:50:07Z"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/matheusdev/workshopmongo/entities/Post.java
+++ src/main/java/com/matheusdev/workshopmongo/entities/Post.java
@@ -2,11 +2,14 @@ package com.matheusdev.workshopmongo.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.matheusdev.workshopmongo.dto.AuthorDTO;
+import com.matheusdev.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
